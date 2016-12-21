733d4f5ae7beb37350f35e76232f1c3e07fd66fe
anooparoor <anoop.a.rao@gmail.com>
2016-12-21 00:37:54 -0500

    add datafile

--- Expend16/create_training_data.py
+++ Expend16/create_training_data.py
@@ -7,15 +7,18 @@ import random
 filename = "expends16.txt"
 
 n = sum(1 for line in open(filename)) - 1
-sample = 10000
+sample = 100000
 
 print n
-
 skip = sorted(random.sample(xrange(n),n-sample))
 df = pd.read_csv(filename, skiprows=skip, quotechar='|')
 df.replace(r'\s+', np.nan,regex=True, inplace=True)
-df.replace('',np.nan, inplace=True) 
-print df.shape
-print df.iloc[10]
+df.replace('',np.nan, inplace=True)
+df.columns=['cycle','id','transid','crpfilterid','recipcode','pacshort','crprecipname','expcode','amount','date','city','state','zip','cmtelid_ef','candid','type','descrip','pg','elecother','enttype','source']
 
-df.to_csv("data.csv")
+df_n = df[df.expcode.notnull() & df.descrip.notnull()]
+ 
+print df_n.shape
+print df_n.iloc[10]
+
+df_n.to_csv("data.csv")
